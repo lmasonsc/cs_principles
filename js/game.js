9639c9f3dfa5674e5025d5c48219c6e32839cd6d
lmasonsc <89035668+lmasonsc@users.noreply.github.com>
2021-10-29 08:33:44 -0700

    10/29 Push

---
+++ js/game.js
@@ -0,0 +1,37 @@
+// global variables
+let canvas;
+let ctx;
+
+// here we use init (short for initialize) to setup the canvas and context
+// this function will be called in the HTML document in body onload = ""
+// we also append the body with a new canvas element
+function init() {
+    canvas = document.createElement("canvas");
+    ctx = canvas.getContext('2d');
+    console.log("Game initialized");
+    document.body.appendChild(canvas);
+    gameLoop();
+}
+
+// we now have just the drawing commands in the function draw
+function draw() {
+    ctx.fillStyle = 'rgb(200, 0, 0)';
+    ctx.fillRect(10, 10, 50, 50);
+    ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+    ctx.fillRect(30, 30, 50, 50);
+}
+
+// here we have a big leap!
+// We are using the window.requestAnimationFrame() 
+// .requestAnimationFrame() is a method (likg a function attached to an object)
+// It tells the browser that you wish to animate
+// It asks the browser to call a specific function, in our case gameLoop
+// It uses this function to 'repaint'
+// In JS this called a callback, where a function passes an argument to another function
+
+// MDN reference https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+let gameLoop = function () {
+    // console.log('the game loop is alive! now comment this out before it eats up memory...');
+    draw();
+    window.requestAnimationFrame(gameLoop);
+}
